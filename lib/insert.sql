2e2e1a34d7413b7aaaf0c958c5294845e9edb925
MakenaG <makenaglory44@gmail.com>
2023-02-20 06:25:01 +0300

    complete

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears(name,age,sex,color,temperament,alive) VALUES("Mr.Chocolate",25,"M","Black","Cool",TRUE);
+INSERT INTO bears(name,age,sex,color,temperament,alive) VALUES("Rowdy",25,"M","Black","Cool",TRUE);
+INSERT INTO bears(name,age,sex,color,temperament,alive) VALUES("Tabitha",25,"F","Black","Cool",TRUE);
+INSERT INTO bears(name,age,sex,color,temperament,alive) VALUES("Sergeant Brown",25,"F","Black","Cool",TRUE);
+INSERT INTO bears(name,age,sex,color,temperament,alive) VALUES("Melissa",25,"F","Black","Cool",TRUE);
+INSERT INTO bears(name,age,sex,color,temperament,alive) VALUES("Grinch",25,"M","Black","Cool",TRUE);
+INSERT INTO bears(name,age,sex,color,temperament,alive) VALUES("Wendy",25,"F","Black","Cool",TRUE);
+INSERT INTO bears(name,age,sex,color,temperament,alive) VALUES(NULL,25,"F","Black","Cool",TRUE);
